d44e4da6be6735707a9a33e5e89279bbcaefb2f0
Mohammed Shamel <160044480+Mohamed-shamel1@users.noreply.github.com>
2025-11-26 17:44:50 +0200

    Ignore ESLint errors during Vercel build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ['three', '@react-three/fiber', '@react-three/drei'],
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
